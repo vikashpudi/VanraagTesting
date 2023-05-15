0792d12b351536099944a9127f5c5e823bd4f7b4
pramod <noolipramodkumar@gmail.com>
2023-05-15 19:06:10 +0530

    made settings change for deployment

--- website/website/settings.py
+++ website/website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bpfq^uj_ev_lcdwf=k%b4c23a=x$+zykgs1$l(i7l4sp2*5sa7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","vanraagtesting.azurewebsites.net"]
 
 
 # Application definition
